72043bf223aab13fe803040bb99ee48e41ef3499
Aditya Dhara <dev@adityadhara.com>
2024-11-24 03:55:04 -0800

    Lets play

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+	"nvim-telescope/telescope.nvim", version='0.1.x',
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-project.nvim",
+	},
+	opts = function()
+		-- extensions
+		require('telescope').load_extension('project')
+
+		-- keymaps
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	end
+}
