27ae2085124d3a9b4cfba0d9b7116cb760c51edb
Aditya Dhara <dev@adityadhara.com>
2024-12-20 23:24:04 -0800

    Added wezterm no more tmux

--- lua/settings.lua
+++ lua/settings.lua
@@ -4,6 +4,7 @@ vim.g.maplocalleader = "\\"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.clipboard = "unnamedplus"
 vim.bo.softtabstop = 2
 
 vim.keymap.set("i", "jk", "<ESC>", { silent = true })
