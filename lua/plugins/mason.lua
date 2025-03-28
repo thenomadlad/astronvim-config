62a88a438025b42e009cb5386e010c362f257295
Aditya Dhara <dev@adityadhara.com>
2025-03-27 23:16:19 -0700

    Moving to blink

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -26,8 +26,6 @@ return {
       handlers = {}
     })
     
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
     require('lspconfig').pylsp.setup({
       capabilities = capabilities,
       settings = {
